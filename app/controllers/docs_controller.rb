d515d381c172581d05fe5e423dc7c7af58c6edea
Litos <windsochi@gmail.com>
2015-01-28 20:59:17 +0400

    add CORS

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,12 +1,30 @@
 class DocsController < ApplicationController
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   def index
     respond_to do |format|
-      format.html {render text: "Data"}
+      format.html {@docs = Doc.all}
       format.json {render json:
         Doc.where(date_of_issue: params[:start]..params[:finish])
         #http://localhost:3000/docs.json?start=2002-01-01&finish=2002-12-31
       }
+    end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
     end
   end
 

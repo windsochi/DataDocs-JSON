2fffe4ef62f17a794cb4226c66af36d9959ad1d8
Litos <windsochi@gmail.com>
2015-01-30 00:15:09 +0400

    add http authenticate

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,14 @@
 class IndexController < ApplicationController
+  before_filter :authenticate
   def index
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "sochi" && password == "sochi"
+    end
   end
 
 end
